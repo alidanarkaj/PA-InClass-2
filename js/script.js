58f247d49f1c55f536b0a494f0aa5d3c6820a4fd
Alida Narkaj <alidanark@gmail.com>
2024-11-14 13:38:32 -0500

    fixed styling

--- js/script.js
+++ js/script.js
@@ -1,19 +1,7 @@
-function showContent(letter) {
-  // Hide all content sections
-  hideAllContent();
-
-  // Display the selected content section
-  document.getElementById(`content-${letter}`).style.display = "block";
+function showFeature(letter) {
+  document.getElementById(`result-${letter}`).classList.remove("hidden");
 }
 
-function hideContent(letter) {
-  // Hide the specific content section
-  document.getElementById(`content-${letter}`).style.display = "none";
-}
-
-function hideAllContent() {
-  // Hide all content sections (A, B, C)
-  document.getElementById("content-A").style.display = "none";
-  document.getElementById("content-B").style.display = "none";
-  document.getElementById("content-C").style.display = "none";
+function hideFeature(letter) {
+  document.getElementById(`result-${letter}`).classList.add("hidden");
 }
